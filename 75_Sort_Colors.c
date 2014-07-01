f7c15f4db4a845194e39598f3b262d9f59d0666b
vagrant <vagrant@ubuntu-14.04-amd64-vbox>
2014-07-01 01:10:58 +0000

    skeleton

---
+++ 75_Sort_Colors.c
@@ -0,0 +1,21 @@
+// Given an array with n objects colored red, white or blue, sort them so that objects of the same color are adjacent, with the colors in the order red, white and blue.
+//Here, we will use the integers 0, 1, and 2 to represent the color red, white, and blue respectively.
+
+// Array, sort, two pointers.
+
+#include<stdio.h>
+
+#define GET_ARRAY_LENGTH(a,l){l=(sizeof(a)/sizeof(a[0]));}
+
+int Sort_Colors(int *a){
+
+
+}
+
+void main(void){
+	int Object[]={1,2,3,1,2,1,3,2,1,2,3,2};
+	if(Sort_Colors(Object))
+		printf("");
+	else
+		printf("Internal Error!");
+}
